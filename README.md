d0e695b29bebd557035e27ce699eccf40fc44e59
VioletGiraffe <alexxxx89@ya.ru>
2019-03-31 22:36:27 +0300

    Sample UEFI boot application (x64 project for MSVC)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UEFI-Bootloader
-A simple UEFI bootloader written in C++ that does not need any third-party support code like Tianocore EDK or gnu-efi; only needs a handful EFI standard definitions that I will provide.
+A simple UEFI bootloader written in C++17 that does not need any third-party support code like Tianocore EDK or gnu-efi; only needs a handful EFI standard definitions that are provided by a sub-module.
+
+
+At the moment, compilation with Microsoft Visual Studio is supported (tested with MSVC 2017). Compilation with clang is also possible.
---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <UEFI/UEFI.h>
+
+EFI_STATUS EFIAPI efi_main(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
+{
+	auto conOut = SystemTable->ConOut;
+
+	conOut->ClearScreen(conOut);
+
+	conOut->SetCursorPosition(conOut, 0, 0);
+	conOut->OutputString(conOut, (const CHAR16*)u"Press any key to type.");
+
+	conOut->SetCursorPosition(conOut, 0, 1);
+	conOut->OutputString(conOut, (const CHAR16*)u"Or press F12 to quit this application.");
+
+	conOut->SetCursorPosition(conOut, 0, 3);
+	conOut->EnableCursor(conOut, TRUE);
+
+	auto conIn = SystemTable->ConIn;
+
+	for (;;)
+	{
+		UINTN index = 0;
+		auto keyEvent = conIn->WaitForKey;
+		SystemTable->BootServices->WaitForEvent(1, &keyEvent, &index);
+
+		EFI_INPUT_KEY key{ 0, 0 };
+		if (conIn->ReadKeyStroke(conIn, &key) != EFI_SUCCESS)
+			continue;
+
+		if (key.UnicodeChar == 0)
+		{
+			// Non-printable character
+			switch (key.ScanCode)
+			{
+			case Cursor_Up:
+				break;
+			case Cursor_Down:
+				break;
+			case Function12:
+				return EFI_SUCCESS;
+			default:
+				break;
+			}
+
+			continue;
+		}
+
+		CHAR16 text[2] = { key.UnicodeChar, L'\0' };
+		conOut->OutputString(conOut, text);
+	}
+
+	return EFI_SUCCESS;
+}
